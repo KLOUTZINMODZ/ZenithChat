58daaf4df4627476b920b66b584f95da046e9ffc
KloutzinModz <wickgames1314@gmail.com>
2025-10-17 06:54:29 -0300

    Klouts testando uns bagui ai

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const logger = require('../utils/logger');
 const User = require('../models/User');
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 
 function requireAdminKey(req, res, next) {
   try {
